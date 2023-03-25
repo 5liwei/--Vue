3e41745ab8d6e3fcbb6fb1f064939a93053a4665
李玮 <505805473@qq.com>
2023-03-25 19:56:55 +0800

    feat:add tabBar

---
+++ jingdong/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe600;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe601;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe602;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe603;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 0.18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
+    }
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+  &__title {
+    font-size: 20px;
+    //缩放字体
+    transform: scale(0.5, 0.5);
+    transform-origin: center top;
+  }
+}
+</style>
